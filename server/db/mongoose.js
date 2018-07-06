bac74f9a40351d357792b833693f34845ba8e605
Pritish Salian <Salianpritish@gmail.com>
2018-07-06 18:25:47 +0530

    Added mlab info to mongoose.js

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,9 +1,10 @@
-
-
+let db = {
+  localhost: 'mongodb://localhost:27017/TodoApp',
+  mlab: 'mongodb://Pritish:salianpl123@ds125381.mlab.com:25381/todoapp'
+};
 var mongoose=require('mongoose');
 
 
 mongoose.Promise=global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
-
+mongoose.connect( db.localhost || db.mlab);
 module.exports={mongoose};
